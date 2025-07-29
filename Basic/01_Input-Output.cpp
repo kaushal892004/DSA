084612220880bf6abda8c053f84121e8bf776b4b
@Kaushal <155300148+kaushal892004@users.noreply.github.com>
2025-07-29 13:13:45 +0530

    Create 01_Input-Output.cpp

---
+++ Basic/01_Input-Output.cpp
@@ -0,0 +1,22 @@
+// Input Output
+
+// Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+
+// Examples:
+// Input(user gives value): 7
+
+// Output: 7
+
+// Input(user gives value): -5
+
+// Output: -5
+
+class Solution {
+public:
+    void printNumber() {
+        int num;
+        cin >> num;
+        cout << num ; 
+
+    }
+};
