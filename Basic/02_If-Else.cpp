e41bd3462d28ce7f82cd4d2a55fc4a96c56fb6b9
@Kaushal <155300148+kaushal892004@users.noreply.github.com>
2025-07-29 13:15:24 +0530

    Create 02_If-Else.cpp

---
+++ Basic/02_If-Else.cpp
@@ -0,0 +1,44 @@
+// If ElseIf
+
+// Given marks of a student, print on the screen:
+
+// Grade A if marks >= 90
+// Grade B if marks >= 70
+// Grade C if marks >= 50
+// Grade D if marks >= 35
+// Fail, otherwise.
+
+// Examples:
+// Input: marks = 95
+
+// Output: Grade A
+
+// Explanation: marks are greater than or equal to 90.
+
+// Input: marks = 14
+
+// Output: Fail
+
+// Explanation: marks are less than 35.
+
+class Solution {
+public:
+    void studentGrade(int marks) {
+        if (marks >= 90){
+            cout << "Grade A";
+        }
+        else if (marks >=70 && marks < 90 ) {
+            cout << "Grade B";
+        }
+        else if (marks >=50 && marks < 70 ) {
+            cout << "Grade C";
+
+        }
+        else if (marks >= 35 && marks < 50 ) {
+            cout << "Grade D";
+        }
+        else{
+            cout << "Fail" ;
+        }
+    }
+};
