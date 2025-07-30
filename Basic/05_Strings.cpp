b3a55aa08992c3c9bc3259d970a15a2cb3133edf
@Kaushal <155300148+kaushal892004@users.noreply.github.com>
2025-07-30 11:08:55 +0530

    Create 05_Strings.cpp

---
+++ Basic/05_Strings.cpp
@@ -0,0 +1,10 @@
+// Problem: Given a string, print it with a greeting
+// Input: name = "Kaushal"
+// Output: Hello, Kaushal!
+
+class Solution {
+public:
+    void greet(string name) {
+        cout << "Hello, " << name << "!" << endl;
+    }
+};
